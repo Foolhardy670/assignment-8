3b17ec0d879b8edea6e40c7c343d1586d08517d1
Christian Simpore <konteysimpore@gmail.com>
2022-11-06 10:28:25 -0800

    assigment 8

---
+++ src/javascripts/gallery.js
@@ -0,0 +1,93 @@
+export const gallery =[{
+    "id": 1,
+    "title": "Tesla",
+    "car" : "https://images.unsplash.com/photo-1617788138017-80ad40651399?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "description" : "Tesla Inc (Tesla) is an automotive and energy company. It designs, develops, manufactures, sells, and leases electric vehicles and energy generation and storage systems."
+
+},
+{
+    "id": 2,
+    "title": "BMW",
+    "car" :"https://images.unsplash.com/flagged/photo-1553505192-acca7d4509be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1790&q=80",
+    "description" : "Today, the BMW Group, with its 31 production and assembly facilities in 15 countries as well as a global sales network, is the world's leading manufacturer of premium automobiles and motorcycles, and provider of premium financial and mobility services."
+
+},
+{
+    "id": 3,
+    "title": "Bentley",
+    "car" : "https://images.unsplash.com/photo-1604946881982-a3321fcf07f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "description" : "From W.O. Bentley, who founded Bentley Motors in 1919, to the current team of over 4,000 dedicated employees, the company’s extraordinary cars have always been designed and built by exceptional people using only the finest of materials. "
+
+},
+{
+    "id": 4,
+    "title": "Ford",
+    "car" :"https://images.unsplash.com/photo-1609361528403-2b64dae24628?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "description" : "Ford Motor Co (Ford) is an automotive company, which designs, manufactures, markets and services a full line of cars, trucks, sport utility vehicles and electrified vehicles. "
+
+},
+{
+    "id": 5,
+    "title": "Honda",
+    "car" :"https://images.unsplash.com/photo-1519641471654-76ce0107ad1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
+    "description" : "Honda Motor Co., Ltd. engages in the manufacture and sale of automobiles, motorcycles, and power products. It operates through the following segments: Automobile, Motorcycle, Financial Services, and Power Product and Other Businesses."
+
+},
+
+{
+    "id": 6,
+    "title": "Mazda",
+    "car" :"https://images.unsplash.com/photo-1599491143816-8c1ea12a4e06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1859&q=80",
+    "description" : "commonly referred to as simply Mazda, is a Japanese multinational automotive manufacturer headquartered in Fuchū, Hiroshima, Japan.[4]"
+
+},
+
+{
+    "id": 7,
+    "title": "Toyota",
+    "car" :"https://images.unsplash.com/photo-1559416523-140ddc3d238c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1751&q=80",
+    "description" : "Toyota Motor Corp. engages in the manufacture and sale of motor vehicles and parts. It operates through the following segments: Automotive, Financial Services, and All Other. "
+
+},
+
+{
+    "id": 8,
+    "title": "Ferrari",
+    "car" :"https://images.unsplash.com/photo-1592198084033-aade902d1aae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "description" : "Ferraris are what dreams are made of. Fast, sexy, and highly exclusive, these Italian supercars push the boundaries of performance and cause a stir wherever they go. "
+
+},
+
+{
+    "id": 9,
+    "title": "Volkswagen",
+    "car" :"https://images.unsplash.com/photo-1572811298797-9eecadf6cb24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "description" : "The Volkswagen brand is one of the world's biggest car manufacturers. We have been making innovative technology, the highest quality and attractive design accessible to many people for more than 70 years now"
+
+},
+
+{
+    "id": 10,
+    "title": "Lamborghini",
+    "car" :"https://images.unsplash.com/photo-1544829099-b9a0c07fad1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
+    "description" : "Automobili Lamborghini S.p.A. (Italian pronunciation: is an Italian brand and manufacturer of luxury sports cars and SUVs based in Sant'Agata Bolognese. "
+
+},
+
+{
+    "id": 11,
+    "title": "Bugatti",
+    "car" :"https://images.unsplash.com/photo-1596809011210-34f77e175150?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "description" : "Automobiles Ettore Bugatti was a German then French manufacturer of high-performance automobiles. The company was founded in 1909 in the then-German city of Molsheim, Alsace, by the Italian-born industrial designer Ettore Bugatti. "
+
+},
+
+{
+    "id": 12,
+    "title": "Rolls-Royce",
+    "car" :"https://images.unsplash.com/photo-1598195221927-fc335f446870?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
+    "description" : "Rolls-Royce PLC, major British manufacturer of aircraft engines, marine propulsion systems, and power-generation systems. Noted for much of the 20th century as a maker of luxury automobiles,"
+
+},
+
+]
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,36 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+import {gallery} from'./gallery'
+
+for (let g of gallery ){
+    let g_images = document.getElementById("m" + g.id)
+    g_images.innerHTML = `
+    <img src="${g.car}" class="img-fluid" alt="${g.title}">
+
+    `
+
+    g_images.onclick = function(){
+        displayCars(g)
+    }
+
+}
+
+let featured_gallery = document.querySelector('.featured')
+function displayCars(galleries){
+    featured_gallery.innerHTML = `
+    <div class="card">
+    <div class="card-header">
+     <h1>${galleries.title}</h1>
+    </div>
+    <img src="${galleries.car}" class="card-img-top" alt="${galleries.title}">
+    <div class="card-body">
+      <h5 class="card-title"><small>${galleries.title} </small></h5>
+      <p class="card-text"> ${galleries.description}</p>
+      
+    </div>
+   
+  </div>
+    `
+}
+
